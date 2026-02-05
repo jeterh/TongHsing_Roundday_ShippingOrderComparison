cd4ebad7102dfef808b646fdeb98ff6c408a7c79
Jeter <jeterh@gmail.com>
2026-02-05 13:52:12 +0800

    調整 vite路徑

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
-  base: '/TongHsing_Roundday_ShippingOrderComparison/', 
+  base: './', // 這樣寫最保險，代表相對路徑
 })
